c2dc055a0c0434c25afc8435a0474f4b07a009b1
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-06-21 17:12:13 +0600

    footer page included

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Footer from "./Pages/SharedPage/Footer";
 import NavBar from "./Pages/SharedPage/NavBar";
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
+      <Footer></Footer>
     </>
   );
 }
