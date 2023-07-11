1c44a47670d17f88812cb10c416eb86e18f6178b
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-07-11 09:06:00 +0600

    routes setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes";
+import Providers from "./AuthProviders/Providers";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Providers>
+      <RouterProvider router={router} />
+    </Providers>
   </React.StrictMode>
 );
